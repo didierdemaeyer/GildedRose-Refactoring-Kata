832fb913409eacd9ee09e087e16156b6231f7f14
Didier De Maeyer <didierdemaeyer@gmail.com>
2020-08-18 00:17:35 +0200

    add old import line

--- TypeScript/test/golden-master-text-test.ts
+++ TypeScript/test/golden-master-text-test.ts
@@ -1,4 +1,5 @@
 import { Item, GildedRose } from '../app/gilded-rose';
+// import { Item, GildedRose } from '../app/gilded-rose-old';
 
 const items = [
     new Item("+5 Dexterity Vest", 10, 20), //
